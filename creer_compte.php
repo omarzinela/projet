3f5d3549bd5af58f42949fe4e3bed76819506bb6
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-10 17:27:28 +0100

    Modification de l'id du champ password appliqué au label.

--- creer_compte.php
+++ creer_compte.php
@@ -28,7 +28,7 @@
         </div>
     </div>
     <div class="form-group row  mt-3">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+        <label for="Password" class="col-sm-2 col-form-label">Password</label>
         <div class="col-sm-10">
             <input type="password" class="form-control" id="Password" placeholder="Password">
         </div>
