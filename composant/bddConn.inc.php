f5cc3de6ac8ea922da9f950257a38bf06a7b0d0a
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-11 02:23:04 +0100

    ajout de set_charset pour que les caractères accentués s'affichent correctement et modification du chemin du fichier credentials.inc.php

--- composant/bddConn.inc.php
+++ composant/bddConn.inc.php
@@ -1,7 +1,8 @@
 <?php
-require_once '../composant/credentials.inc.php'; // Le fichier DOIS être dans le git ignore
+require_once 'credentials.inc.php'; // Le fichier DOIT être dans le git ignore
 // Create connection
 $conn = new mysqli($host, $login, $password, $dbname);
+$conn->set_charset("utf8mb4"); // Sinon les accents ne s'affichent pas correctement
 // Check connection
 if ($conn->connect_error) {
     die("Échec de connection bdd: " . $conn->connect_error);
