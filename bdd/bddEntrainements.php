13cd75141dd6d8e77fc2e1148138146d74027bca
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-11 02:24:27 +0100

    Modification de la page des courses pour afficher une liste des courses depuis la bdd.

--- bdd/bddEntrainements.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once '../composant/bddConn.inc.php';
-
-$res = $conn->query("select * from Entrainements");
-    while ($row = $res->fetch_assoc()) {
--- entrainements.php
+++ entrainements.php
@@ -1,20 +1,22 @@
 <?php include_once 'composant/header.php'; ?>
 
-<?php include_once 'bdd/bddEntrainements.php';?>
+<?php include_once 'composant/bddConn.inc.php';
 
-<ul class="list-group list-group-flush flex-row border rounded mt-2">
-    <li class="list-group-item" style="width: 290px; height: 250px;">Entraînement 1:
+$res = $conn->query("select * from Entrainements");
+while ($row = $res->fetch_assoc()) {
+
+echo '<ul class="list-group list-group-flush flex-row border rounded mt-2">
+    <li class="list-group-item" style="width: 290px; height: 250px;">' . $row['EntrainementNom'] . ':
         <ul>
-        <li><?= $row['Date'] ?></li>
-        <li><?= $row['Heure'] ?></li>
-        <li><?= $row['Lieu'] ?></li>
-        <li><?= $row['Categorie'] ?></li>
-        <li><?= $row['Description'] ?></li>
+        <li>' . date('Y/m/d',$row['EntrainementTimestamp']) . '</li>
+        <li>' . date('H:i',$row['EntrainementTimestamp']) .'</li>
+        <li>' . $row['Categorie']. '</li>
+        <li>' . $row['Description'] . '</li>
         </ul>
         <div class="bottom" style="position: absolute;bottom: 0;">
-            <button type="submit" class="btn btn-color mb-2">S'inscrire</button>
+            <button type="submit" class="btn btn-color mb-2">S\'inscrire</button>
             <button type="submit" class="btn btn-color mb-2">
-                <a href="information.php" style="color: white; text-decoration:none;"> Plus d'informations
+                <a href="information.php" style="color: white; text-decoration:none;"> Plus d\'informations
                 </a>
             </button>
         </div>
@@ -22,11 +24,12 @@
     </li>
     <div class="box mt-4" style="margin-left: auto;">
         <div class="div" style="margin-left: auto;">
-            <img src="img/depositphotos_54892607-stock-photo-male-runner-in-san-francisco.jpg" alt="" style="width: 300px;">
+            <img src="' . $row['EntrainementThumbnail'] . '" alt="" style="width: 300px;">
         </div>
     </div>
-</ul>
+</ul>';
 
-<?php}?>
+}
+?>
 
-<?php include_once 'composant/footer.php'; ?>
+<?php include_once 'composant/footer.php';
