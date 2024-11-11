d0bfd0eb56bd6e8e32ae995183eec8c79cbc21f9
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-11 02:52:24 +0100

    Ajout gestion erreur et bdd vide.

--- entrainements.php
+++ entrainements.php
@@ -3,32 +3,36 @@
 <?php include_once 'composant/bddConn.inc.php';
 
 $res = $conn->query("select * from Entrainements");
-while ($row = $res->fetch_assoc()) {
+if (!$res) {
+    echo 'Échec requête: ' . $conn->error;
+} elseif ($res->num_rows == 0) {
+    echo '<p>Aucunes courses dans la bdd</p>';
+} else {
+    while ($row = $res->fetch_assoc()) {
 
-echo '<ul class="list-group list-group-flush flex-row border rounded mt-2">
-    <li class="list-group-item" style="width: 290px; height: 250px;">' . $row['EntrainementNom'] . ':
-        <ul>
-        <li>' . date('Y/m/d',$row['EntrainementTimestamp']) . '</li>
-        <li>' . date('H:i',$row['EntrainementTimestamp']) .'</li>
-        <li>' . $row['Categorie']. '</li>
-        <li>' . $row['Description'] . '</li>
-        </ul>
-        <div class="bottom" style="position: absolute;bottom: 0;">
-            <button type="submit" class="btn btn-color mb-2">S\'inscrire</button>
-            <button type="submit" class="btn btn-color mb-2">
-                <a href="information.php" style="color: white; text-decoration:none;"> Plus d\'informations
-                </a>
-            </button>
-        </div>
+        echo '<ul class="list-group list-group-flush flex-row border rounded mt-2">
+            <li class="list-group-item" style="width: 290px; height: 250px;">' . $row['EntrainementNom'] . ':
+                <ul>
+                    <li>' . date('Y/m/d',$row['EntrainementTimestamp']) . '</li>
+                    <li>' . date('H:i',$row['EntrainementTimestamp']) .'</li>
+                    <li>' . $row['Categorie']. '</li>
+                    <li>' . $row['Description'] . '</li>
+                </ul>
+                <div class="bottom" style="position: absolute;bottom: 0;">
+                    <button type="submit" class="btn btn-color mb-2">S\'inscrire</button>
+                    <button type="submit" class="btn btn-color mb-2">
+                        <a href="information.php" style="color: white; text-decoration:none;"> Plus d\'informations</a>
+                    </button>
+                </div>
 
-    </li>
-    <div class="box mt-4" style="margin-left: auto;">
-        <div class="div" style="margin-left: auto;">
-            <img src="' . $row['EntrainementThumbnail'] . '" alt="" style="width: 300px;">
-        </div>
-    </div>
-</ul>';
-
+            </li>
+            <div class="box mt-4" style="margin-left: auto;">
+                <div class="div" style="margin-left: auto;">
+                    <img src="' . $row['EntrainementThumbnail'] . '" alt="" style="width: 300px;">
+                </div>
+            </div>
+        </ul>';
+    }
 }
 ?>
 
