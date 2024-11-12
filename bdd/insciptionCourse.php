9b1b3dbf7e45355c39fd5ad413318bc641394f37
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-12 21:09:31 +0100

    Ajout fonctionnalité inscription

---
+++ bdd/insciptionCourse.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../composant/bddConn.inc.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $EntrainementId = $_REQUEST['EntrainementId'];
+}
+$time = time();
+
+$stmt = $conn->prepare("insert into Inscriptions (UtilisateurId, EntrainementId, InscriptionTimestamp) values (?, ?, ?)");
+$stmt->bind_param('ssi',$_SESSION['UtilisateurId'], $EntrainementId, $time);
+
+if (!$stmt->execute()) {
+    $_SESSION['Msg'] = "Échec exécution requête: " . $stmt->error;
+} else {
+    $_SESSION['Msg'] = "Inscription réussie!";
+}
+
+// Closing the connection.
+$conn->close();
+
+header('Location: '."../entrainements.php");
--- entrainements.php
+++ entrainements.php
@@ -1,10 +1,18 @@
 <?php
 include_once 'composant/header.php';
 require_once 'composant/bddConn.inc.php';
+include_once 'composant/notification.php';
+
+$resIns = $conn->query("select IdEntrainnement from Inscriptions where IdUtilisateur =" . $_SESSION['UtilisateurId']); // Risque d'injection mais si l'utilisateur manipule son cookie il peut devenir admin plus facilement
+if (!$res) {
+    echo '<p>Échec requête: ' . $conn->error . '</p>';
+} else {
+    $inscriptions = $resIns->fetch_all();
+}
 
 $res = $conn->query("select * from Entrainements");
 if (!$res) :
-    echo 'Échec requête: ' . $conn->error;
+    echo '<p>Échec requête: ' . $conn->error . '</p>';
 elseif ($res->num_rows == 0) :
     echo '<p>Aucunes course dans la bdd</p>';
 else :
@@ -15,13 +23,13 @@ else :
     <li class="list-group-item" style="width: 290px; height: 250px;"> <?php echo $row['EntrainementNom'] ?> :
         <ul>
             <li> <?php echo date('Y/m/d',$row['EntrainementTimestamp']) ?> </li>
-            <li>' <?php echo date('H:i',$row['EntrainementTimestamp']) ?> '</li>
-            <li>' <?php echo $row['Categorie'] ?> '</li>
-            <li>' <?php echo $row['Description'] ?> '</li>
+            <li> <?php echo date('H:i',$row['EntrainementTimestamp']) ?> </li>
+            <li> <?php echo $row['Categorie'] ?> </li>
+            <li> <?php echo $row['Description'] ?> </li>
         </ul>
     <div class="bottom" style="position: absolute;bottom: 0;">
-        <form action="bdd/inscription.php" method = "POST">
-            <button type="submit" name="EntrainementId" value=" <?php echo $row['EntrainementId'] ?> " class="btn btn-color mb-2">S'inscrire</button>
+        <form action="bdd/inscriptionCourse.php" method = "POST">
+            <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" <?php if (in_array($row['EntrainementId'],$inscriptions,true)){ ?> disabled <?php   } ?> class="btn btn-color mb-2">S'inscrire</button>
         </form>
     </div>
     </li>
