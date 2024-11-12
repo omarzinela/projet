22ed8dc2c51b7f64023cfd28d90174e1713c5c39
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-12 15:38:59 +0100

    fixed bug where wrong stylesheet would be loaded

--- composant/header.php
+++ composant/header.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" href="img/logo-reshaped.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style31.css">
+    <link rel="shortcut icon" href="../img/logo-reshaped.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 
     <title>RUNNINESIGELEC</title>
@@ -19,7 +18,7 @@
         <header class="">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <a class="navbar-brand" href="index.php">
-                    <!-- <img src="img/Logo_bannière.png" alt=""> -->
+                    <!-- <img src="../img/Logo_bannière.png" alt=""> -->
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -58,3 +57,4 @@
         <main style="
     min-height: 600px;
 ">
+<?php session_start()?>
--- composant/header_user.php
+++ composant/header_user.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style31.css">
     <link rel="shortcut icon" href="img/logo-reshaped.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
