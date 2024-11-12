4f548a5a292bc2f28b67cc203f9489a3a805c1f1
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-12 18:17:17 +0100

    Ajout page et système de connection

---
+++ bdd/bddConnexion.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require_once '../composant/bddConn.inc.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Récupérer les valeurs du form
+    $Email = $_REQUEST['Email'];
+    $Password = $_REQUEST['Password'];
+} else {
+    header('../index.php'); // Rediriger vers page principale si l'utilisateur n'arrive pas du form
+}
+
+if (!($stmt = $conn->prepare("select UtilisateurId, Pass, EstMembre from Utilisateurs where Mail = ?"))) {
+    $_SESSION['Msg'] = "Echec de la préparation : (" . $conn->errno . ") " . $conn->error;
+}
+$stmt->bind_param('s', $Email);
+
+if (!$stmt->execute()) {
+    $_SESSION['Msg'] = "Échec exécution requête: " . $stmt->error;
+    $redirUrl = "../se_connecter.php";
+
+} else {
+    $stmt->store_result();
+    if ($stmt->num_rows() == 0) {
+        $_SESSION['Msg'] = 'Informations de connection invalides!';
+        $redirUrl = "../se_connecter.php";
+    }
+    else {
+        $stmt->bind_result($UtilisateurId, $PasswordHash, $EstMembre);
+        $stmt->fetch();
+        if (password_verify($Password,$PasswordHash)) {
+            $_SESSION['UtilisateurId'] = $UtilisateurId;
+            $_SESSION['EstMembre'] = $EstMembre; // Il faut utiliser database session sinon manipulation de cookie triviale
+            $_SESSION['Msg'] = 'Connexion réussie!';
+            $redirUrl = "../index.php";
+
+        } else {
+            $_SESSION['Msg'] = 'Informations de connection invalides!';
+            $redirUrl = "../se_connecter.php";
+        }
+    }
+}
+unset($Password);
+$conn->close();
+header('Location: '. $redirUrl);
--- se_connecter.php
+++ se_connecter.php
@@ -1,21 +1,22 @@
-<?php include_once 'composant/header.php'; ?>
 
-<form>
+<?php include_once 'composant/header.php';
+include_once 'composant/notification.php'; ?>
 
+<form action="bdd/bddConnexion.php" method="POST">
     <div class="form-group row  mt-3">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <label for="Email" class="col-sm-2 col-form-label">Email</label>
         <div class="col-sm-10">
-            <input type="mail" class="form-control" id="Prenom" placeholder="email@groupe-esigelec.org">
+            <input type="mail" class="form-control" id="Email" name="Email" placeholder="email@groupe-esigelec.org" required>
 
         </div>
     </div>
     <div class="form-group row  mt-3">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Mot de passe</label>
+        <label for="Password" class="col-sm-2 col-form-label">Mot de passe</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
+            <input type="password" class="form-control" id="Password" name="Password" placeholder="Mot de passe" required>
         </div>
     </div>
-    <button type="submit" class="btn btn-color mb-2">Créer un Compte</button>
+    <button type="submit" class="btn btn-color mb-2">Connexion</button>
 
 </form>
 
