f674ced992ef9490798f2f7ffb09d65afb930c97
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-13 11:21:51 +0100

    Renommage de EstMembre en EstAdmin car c'est la fonction réelle de la variable

--- bdd/bddConnexion.php
+++ bdd/bddConnexion.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header('../index.php'); // Rediriger vers page principale si l'utilisateur n'arrive pas du form
 }
 
-if (!($stmt = $conn->prepare("select UtilisateurId, Pass, EstMembre from Utilisateurs where Mail = ?"))) {
+if (!($stmt = $conn->prepare("select UtilisateurId, Pass, EstAdmin from Utilisateurs where Mail = ?"))) {
     $_SESSION['Msg'] = "Echec de la préparation : (" . $conn->errno . ") " . $conn->error;
 }
 $stmt->bind_param('s', $Email);
@@ -27,11 +27,11 @@ if (!$stmt->execute()) {
         $redirUrl = "../se_connecter.php";
     }
     else {
-        $stmt->bind_result($UtilisateurId, $PasswordHash, $EstMembre);
+        $stmt->bind_result($UtilisateurId, $PasswordHash, $EstAdmin);
         $stmt->fetch();
         if (password_verify($Password,$PasswordHash)) {
             $_SESSION['UtilisateurId'] = $UtilisateurId;
-            $_SESSION['EstMembre'] = $EstMembre; // Il faut utiliser database session sinon manipulation de cookie triviale
+            $_SESSION['EstAdmin'] = $EstAdmin; // Il faut utiliser database session sinon manipulation de cookie triviale
             $_SESSION['Msg'] = 'Connexion réussie!';
             $redirUrl = "../index.php";
 
