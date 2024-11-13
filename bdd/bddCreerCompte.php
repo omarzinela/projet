454d9e84c8b9784a068da0bd89b2c910b979afa3
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-13 10:47:48 +0100

    reflected phone number removal on form

--- bdd/bddCreerCompte.php
+++ bdd/bddCreerCompte.php
@@ -5,15 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les valeurs du form
     $Nom = $_REQUEST['Nom'];
     $Prenom = $_REQUEST['Prenom'];
-    $Numero = $_REQUEST['Numero'];
     $Email = $_REQUEST['Email'];
     $Password = password_hash($_REQUEST['Password'], PASSWORD_BCRYPT);
 }
 
 require_once '../composant/bddConn.inc.php';
 
-$stmt = $conn->prepare("insert into Utilisateurs (Nom, Prenom, Mail, Tel, Pass) values (?, ?, ?, ?, ?)");
-$stmt->bind_param('sssss',$Nom, $Prenom, $Email, $Numero, $Password);
+$stmt = $conn->prepare("insert into Utilisateurs (Nom, Prenom, Mail, Pass) values (?, ?, ?, ?)");
+$stmt->bind_param('ssss',$Nom, $Prenom, $Email, $Password);
 if (!$stmt->execute()) {
     $_SESSION["Msg"] = "Échec exécution requête: " . $stmt->error;
 } else {
