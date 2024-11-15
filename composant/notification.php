0315ef5d4aa86eff7ce07fe13b54a495ffdeac6a
omar <omar.zinelabidine@groupe-esigelec.org>
2024-11-16 00:39:04 +0100

    ajout des notifications avec mise en page différentes selon le contexte

--- composant/notification.php
+++ composant/notification.php
@@ -1,3 +1,19 @@
 <?php if (isset($_SESSION['Msg'])): ?>
+<div class="alert alert-success" role="alert">
+  Ceci est une alerte de succès.
+</div>
+
+<div class="alert alert-danger" role="alert">
+  Ceci est une alerte de danger.
+</div>
+
+<div class="alert alert-warning" role="alert">
+  Ceci est une alerte de mise en garde.
+</div>
+
+<div class="alert alert-info" role="alert">
+  Ceci est une alerte d'information.
+</div>
+
 <?php echo $_SESSION['Msg']; // todo: faire une notification jolie avec bootstrap en insérant cette ligne (moins le commentaire) dans le html ?>
 <?php endif;?>
