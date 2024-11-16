f9dc842c0bf4b0e91b010ce2574201dd3725914e
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-16 13:52:12 +0100

    Ajout du lieu à la liste des entraînements + modification de l'affichage sélectif des informations pour ne pas avoir d'élément vide dans la liste à puces + modification du bouton inscription en inscription/désinscription

--- bdd/bddEntrainements.php
+++ bdd/bddEntrainements.php
@@ -5,7 +5,7 @@ if(isset($_SESSION['UtilisateurId'])) {
     $stmt = $conn->prepare("select EntrainementId from Inscriptions where UtilisateurId = ?");
     $stmt->bind_param('s', $_SESSION['UtilisateurId']);
     if (!$stmt->execute()) {
-        $_SESSION["Msg"] = "Échec exécution requête: " . $stmt->error;
+        $_SESSION["Err"] = "Échec exécution requête: " . $stmt->error;
     } else {
         $resIns = $stmt->get_result();
         $inscriptions = $resIns->fetch_all(MYSQLI_ASSOC);
--- entrainements.php
+++ entrainements.php
@@ -4,9 +4,9 @@ include_once 'composant/notification.php';
 require_once 'bdd/bddEntrainements.php';
 
 if (!$res) :
-    $_SESSION["Msg"] = '<p>Échec requête: ' . $conn->error . '</p>';
+    $_SESSION["Err"] = '<p>Échec requête: ' . $conn->error . '</p>';
 elseif ($res->num_rows == 0) :
-    $_SESSION["Msg"] = '<p>Aucunes course dans la bdd</p>';
+    $_SESSION["Info"] = '<p>Aucunes course dans la bdd</p>';
 else :
     while ($row = $res->fetch_assoc()) :
 ?>
@@ -14,17 +14,27 @@ else :
 <ul class="list-group list-group-flush flex-row border rounded mt-2">
     <li class="list-group-item" style="width: 290px; height: 250px;"> <?php echo $row['EntrainementNom']; ?> :
         <ul>
-            <li> <?php if (isset($_SESSION['UtilisateurId'])) echo date('Y/m/d',$row['EntrainementTimestamp']); ?> </li>
-            <li> <?php if (isset($_SESSION['UtilisateurId'])) echo date('H:i',$row['EntrainementTimestamp']); ?> </li>
+        <?php if (isset($_SESSION['UtilisateurId'])): ?>
+            <li> <?php echo date('Y/m/d',$row['EntrainementTimestamp']); ?> </li>
+            <li> <?php echo date('H:i',$row['EntrainementTimestamp']); ?> </li>
+            <li> <?php echo $row['Lieu']; ?> </li>
+        <?php endif;?>
             <li> <?php echo $row['Categorie']; ?> </li>
             <li> <?php echo $row['Description']; ?> </li>
         </ul>
+        <?php if (isset($_SESSION['UtilisateurId'])): ?>
     <div class="bottom" style="position: absolute;bottom: 0;">
         <form action="bdd/inscriptionCourse.php" method = "POST">
-            <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" <?php if (in_array($row['EntrainementId'],array_column($inscriptions,'EntrainementId'),true)){ ?> disabled <?php   } ?> class="btn btn-color mb-2">S'inscrire</button>
-            <!-- todo: remplacer par un switch inscription/désinscription -->
+        <?php if (in_array($row['EntrainementId'],array_column($inscriptions,'EntrainementId'),true)): ?>
+            <input type="hidden" name="btnState" value="delete"/>
+            <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-color mb-2">Se désinscrire</button>
+        <?php else: ?>
+            <input type="hidden" name="btnState" value="create"/>
+            <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-color mb-2">S'inscrire</button>
+        <?php endif;?>
         </form>
     </div>
+    <?php endif;?>
     </li>
     <div class="box mt-4 div_auto">
         <div class="div div_auto">
