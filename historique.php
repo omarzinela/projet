f6dab781d0f458a9b3723262b512857f3bff6cd6
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-16 17:23:09 +0100

    Ajout page historique pour voir les entraînements passés

--- historique.php
+++ historique.php
@@ -1,28 +1,37 @@
-<?php include_once 'composant/header.php'; ?>
+<?php
+include_once 'composant/header.php';
+include_once 'composant/notification.php';
+require_once 'bdd/bddHistorique.php';
+
+if (!$res) :
+    $_SESSION["Err"] = '<p>Échec requête: ' . $conn->error . '</p>';
+elseif ($res->num_rows == 0) :
+    $_SESSION["Info"] = '<p>Aucunes course dans la bdd</p>';
+else :
+    while ($row = $res->fetch_assoc()) :
+?>
 
 <ul class="list-group list-group-flush flex-row border rounded mt-2">
-    <li class="list-group-item" style="width: 290px; height: 250px;">Entraînement 7:
-        <ul>
-        <li>Date</li>
-        <li>Heure</li>
-        <li>Lieu</li>
-        <li>Catégorie</li>
-        <li>Description</li>
+    <li class="list-group-item d-flex flex-column" style="width: 290px; height: 250px;">
+        <div>
+        <?php echo $row['EntrainementNom']; ?> :
+        <ul class="mb-0">
+            <li> <?php echo date('Y/m/d',$row['EntrainementTimestamp']); ?> </li>
+            <li> <?php echo date('H:i',$row['EntrainementTimestamp']); ?> </li>
+            <li> <?php echo $row['Lieu']; ?> </li>
+            <li> <?php echo $row['Categorie']; ?> </li>
+            <li> <?php echo $row['Description']; ?> </li>
         </ul>
-        <div class="bottom" style="position: absolute;bottom: 0;">
-            <button type="submit" class="btn btn-color mb-2">
-                <a href="information.php" style="color: white; text-decoration:none;"> Plus d'informations
-                </a>
-            </button>
         </div>
-
     </li>
+
     <div class="box mt-4 div_auto">
         <div class="div div_auto">
-            <img src="img/depositphotos_54892607-stock-photo-male-runner-in-san-francisco.jpg" alt="" style="width: 300px;
-">
+            <img src=" <?php echo $row['EntrainementThumbnail'] ?>" alt="Miniature de l'entraînement" style="width: 300px;">
         </div>
     </div>
 </ul>
 
-<?php include_once 'composant/footer.php'; ?>
+<?php endwhile;
+endif;
+include_once 'composant/footer.php';
