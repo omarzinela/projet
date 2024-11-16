0c901d46c6a116fccb157b00b5c5c2fb8ec9d3c9
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-16 15:14:20 +0100

    Fixed button layout

--- entrainements.php
+++ entrainements.php
@@ -12,8 +12,10 @@ else :
 ?>
 
 <ul class="list-group list-group-flush flex-row border rounded mt-2">
-    <li class="list-group-item" style="width: 290px; height: 250px;"> <?php echo $row['EntrainementNom']; ?> :
-        <ul>
+    <li class="list-group-item d-flex flex-column" style="width: 290px; height: 250px;">
+        <div>
+        <?php echo $row['EntrainementNom']; ?> :
+        <ul class="mb-0">
         <?php if (isset($_SESSION['UtilisateurId'])): ?>
             <li> <?php echo date('Y/m/d',$row['EntrainementTimestamp']); ?> </li>
             <li> <?php echo date('H:i',$row['EntrainementTimestamp']); ?> </li>
@@ -22,20 +24,22 @@ else :
             <li> <?php echo $row['Categorie']; ?> </li>
             <li> <?php echo $row['Description']; ?> </li>
         </ul>
+        </div>
+    </li>
         <?php if (isset($_SESSION['UtilisateurId'])): ?>
-    <div class="bottom" style="position: absolute;bottom: 0;">
+    <div class="mt-auto">
         <form action="bdd/inscriptionCourse.php" method = "POST">
         <?php if (in_array($row['EntrainementId'],array_column($inscriptions,'EntrainementId'),true)): ?>
-            <input type="hidden" name="btnState" value="delete"/>
+            <input type="hidden" name="btnState" value="delete">
             <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-color mb-2">Se désinscrire</button>
         <?php else: ?>
-            <input type="hidden" name="btnState" value="create"/>
+            <input type="hidden" name="btnState" value="create">
             <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-color mb-2">S'inscrire</button>
         <?php endif;?>
         </form>
     </div>
     <?php endif;?>
-    </li>
+
     <div class="box mt-4 div_auto">
         <div class="div div_auto">
             <img src=" <?php echo $row['EntrainementThumbnail'] ?>" alt="Miniature de l'entraînement" style="width: 300px;">
