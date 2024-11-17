b5a6c386cc0ba9dcd93bd72116f45d0160c9cad6
omar <omar.zinelabidine@groupe-esigelec.org>
2024-11-17 18:43:13 +0100

    update page a propos avec les style correspondant

--- a_propos.php
+++ a_propos.php
@@ -1,6 +1,7 @@
 <?php include_once 'composant/header.php'; ?>
-
-<ul class="list-group list-group-flush flex-row rounded mt-2 a_propos_paragraphe" >
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-12 col-md-6 mb-4">
 
     <p class="texte_a_propos"> À RUNNINESIGELEC, nous sommes passionnés par la course à pied. Notre mission est de créer une communauté où chacun peut partager ses expériences, améliorer ses compétences, et s'inspirer pour atteindre ses objectifs personnels.
 
@@ -15,13 +16,15 @@ Respect : Chaque individu est accueilli avec bienveillance et sans jugement.
 Innovation : Nous explorons constamment de nouvelles façons d'améliorer nos services.
 Nous croyons que c'est grâce à l'esprit de collaboration et au respect mutuel que nous pouvons accomplir de grandes choses. Rejoignez-nous dans cette aventure et contribuez à façonner un avenir plein de possibilités !
     </p>
-
-
-    <div class="box mt-4 div_auto">
-        <div class="div div_auto">
-            <img class="img_big" src="img/etirement.jpg" alt="">
-        </div>
     </div>
 
-</ul>
+
+    <div class="col-12 col-md-6">
+            <div class="text-center">
+                <img class="img-fluid img_big" src="img/etirement.jpg" alt="Image d'étirement">
+            </div>
+        </div>
+    </div>
+</div>
+    
 <?php include_once 'composant/footer.php'; ?>
