a68b5b395d6f4aa32656d1c75a7384cc2b9a2908
omar <omar.zinelabidine@groupe-esigelec.org>
2024-11-17 20:01:15 +0100

    formulaires adaptes a la version mobile

--- se_connecter.php
+++ se_connecter.php
@@ -3,21 +3,21 @@
 include_once 'composant/notification.php'; ?>
 
 <form action="bdd/bddConnexion.php" method="POST">
-    <div class="form-group row  mt-3">
-        <label for="Email" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-            <input type="mail" class="form-control" id="Email" name="Email" placeholder="email@groupe-esigelec.org" required>
-
+    <div class="form-group row mt-3">
+        <label for="Email" class="col-12 col-sm-2 col-form-label">Email</label>
+        <div class="col-12 col-sm-10">
+            <input type="email" class="form-control" id="Email" name="Email" placeholder="email@groupe-esigelec.org" required>
         </div>
     </div>
-    <div class="form-group row  mt-3">
-        <label for="Password" class="col-sm-2 col-form-label">Mot de passe</label>
-        <div class="col-sm-10">
+
+    <div class="form-group row mt-3">
+        <label for="Password" class="col-12 col-sm-2 col-form-label">Mot de passe</label>
+        <div class="col-12 col-sm-10">
             <input type="password" class="form-control" id="Password" name="Password" placeholder="Mot de passe" required>
         </div>
     </div>
-    <button type="submit" class="btn btn-color mb-2">Connexion</button>
 
+    <button type="submit" class="btn btn-color mb-2 w-100 w-md-auto">Connexion</button>
 </form>
 
 <?php include_once 'composant/footer.php'; ?>
