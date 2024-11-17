dc5b715ddd79bcf9809611f139c0a13a06940584
omar <omar.zinelabidine@groupe-esigelec.org>
2024-11-17 12:15:35 +0100

    changement password en mot de passe

--- creer_compte.php
+++ creer_compte.php
@@ -23,9 +23,9 @@
         </div>
     </div>
     <div class="form-group row  mt-3">
-        <label for="Password" class="col-sm-2 col-form-label">Password</label>
+        <label for="Password" class="col-sm-2 col-form-label">Mot de passe</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="Password" name="Password" placeholder="Password">
+            <input type="password" class="form-control" id="Password" name="Mot de passe" placeholder="Mot de passe">
         </div>
     </div>
     <button type="submit" class="btn btn-color mb-2">Créer un Compte</button>
