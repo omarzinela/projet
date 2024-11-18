8b45a683acdacf5199e47a0844f49239ea6dbb38
omar <omar.zinelabidine@groupe-esigelec.org>
2024-11-18 15:57:04 +0100

    changement taille des cardes entrainement

--- entrainements.php
+++ entrainements.php
@@ -16,38 +16,36 @@ else :
         <?php while ($row = $res->fetch_assoc()) : ?>
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="card h-100 border rounded">
-                    <img src="<?php echo $row['EntrainementThumbnail']; ?>" alt="Miniature de l'entraînement" class="card-img-top img-fluid entrainement" >
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $row['EntrainementNom']; ?></h5>
-                        <ul class="list-unstyled mb-0">
+                    <img src="<?php echo $row['EntrainementThumbnail']; ?>" alt="Miniature de l'entraînement" class="card-img-top img-fluid" style="height: 180px; object-fit: cover;">
+                    <div class="card-body p-2">
+                        <h6 class="card-title mb-1"><?php echo $row['EntrainementNom']; ?></h6>
+                        <ul class="list-unstyled mb-1">
                             <?php if (isset($_SESSION['UtilisateurId'])): ?>
-                                <li>Date: <?php echo date('Y/m/d', $row['EntrainementTimestamp']); ?></li>
-                                <li>Heure: <?php echo date('H:i', $row['EntrainementTimestamp']); ?></li>
-                                <li>Lieu: <?php echo $row['Lieu']; ?></li>
+                                <li class="small">Date: <?php echo date('Y/m/d', $row['EntrainementTimestamp']); ?></li>
+                                <li class="small">Heure: <?php echo date('H:i', $row['EntrainementTimestamp']); ?></li>
+                                <li class="small">Lieu: <?php echo $row['Lieu']; ?></li>
                             <?php endif; ?>
-                            <li>Catégorie: <?php echo $row['Categorie']; ?></li>
-                            <li>Description: <?php echo $row['Description']; ?></li>
+                            <li class="small">Catégorie: <?php echo $row['Categorie']; ?></li>
+                            <li class="small">Description: <?php echo $row['Description']; ?></li>
                         </ul>
                     </div>
-                    <div class="card-footer text-center">
+                    <div class="card-footer p-2 text-center">
                         <?php if (isset($_SESSION['UtilisateurId'])): ?>
                             <form action="bdd/inscriptionCourse.php" method="POST">
                                 <?php if (in_array($row['EntrainementId'], array_column($inscriptions, 'EntrainementId'), true)): ?>
                                     <input type="hidden" name="btnState" value="delete">
-                                    <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId']; ?>" class="btn btn-danger mb-2">Se désinscrire</button>
+                                    <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId']; ?>" class="btn btn-sm btn-danger mb-1">Se désinscrire</button>
                                 <?php elseif (@$nbParti[$row['EntrainementId']] < $row['MaxParticipants']): ?>
                                     <input type="hidden" name="btnState" value="create">
-                                    <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId']; ?>" class="btn btn-success mb-2">S'inscrire</button>
+                                    <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId']; ?>" class="btn btn-sm btn-success mb-1">S'inscrire</button>
                                 <?php endif; ?>
                                 <input type="hidden" name="source" value="../entrainements.php">
                             </form>
                         <?php endif; ?>
                         <?php if (@$_SESSION['EstAdmin']): ?>
-                            <div>
-                                <form action="listeUtilisateurs.php" method = "POST">
-                                    <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId']; ?>" class="btn btn-color mb-2">Liste inscrits</button>
-                                </form>
-                            </div>
+                            <form action="listeUtilisateurs.php" method="POST">
+                                <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId']; ?>" class="btn btn-sm btn-color mb-1">Liste inscrits</button>
+                            </form>
                         <?php endif; ?>
                     </div>
                 </div>
