b89cdda47ea497c04a648e56b6f8cb9b964d0d2b
Antoine-Mugnier <antoine.mugnier@groupe-esigelec.org>
2024-11-18 15:52:36 +0100

    Added email domain check

--- bdd/bddCreerCompte.php
+++ bdd/bddCreerCompte.php
@@ -6,28 +6,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $Nom = $_REQUEST['Nom'];
     $Prenom = $_REQUEST['Prenom'];
     $Email = $_REQUEST['Email'];
+    $domain = explode('@',$Email)[1];
     $Password = password_hash($_REQUEST['Password'], PASSWORD_BCRYPT);
 } else {
     header('Location:../index.php'); // Rediriger vers page principale si l'utilisateur n'arrive pas du form
 }
 
-require_once dirname(__FILE__).'/../composant/bddConn.inc.php';
+if (strcmp($domain, 'groupe-esigelec.org') != 0) { // Authoriser uniquement les adresses esigelec
+    $_SESSION['Warn'] = 'Uniquement les emails @groupe-esigelec.org sont authorisés!';
+    header('Location:../creer_compte.php');
+} else {
 
-$stmt = $conn->prepare("insert into Utilisateurs (Nom, Prenom, Mail, Pass) values (?, ?, ?, ?)");
-$stmt->bind_param('ssss',$Nom, $Prenom, $Email, $Password);
-try {
-    $stmt->execute();
-} catch (mysqli_sql_exception $e) {
-    if($stmt->errno == 1062) {
-        $_SESSION["Err"] = "Email déjà utilisé";
-    } else {
-        $_SESSION["Err"] = "Échec exécution requête: " . $stmt->error;
+    require_once dirname(__FILE__).'/../composant/bddConn.inc.php';
+
+    $stmt = $conn->prepare("insert into Utilisateurs (Nom, Prenom, Mail, Pass) values (?, ?, ?, ?)");
+    $stmt->bind_param('ssss',$Nom, $Prenom, $Email, $Password);
+    try {
+        $stmt->execute();
+    } catch (mysqli_sql_exception $e) {
+        if($stmt->errno == 1062) {
+            $_SESSION["Err"] = "Email déjà utilisé";
+        } else {
+            $_SESSION["Err"] = "Échec exécution requête: " . $stmt->error;
+        }
+        $redir = '../creer_compte.php';
     }
-}
-if(!isset($_SESSION["Err"])) {
-    $_SESSION["Msg"] = "Création de compte réussie!";
-}
+    if(!isset($_SESSION["Err"])) {
+        $_SESSION["Msg"] = "Création de compte réussie!";
+        $redir = '../index.php';
+    }
 
-// Closing the connection.
-$conn->close();
-header('Location:'."../index.php");
+    // Closing the connection.
+    $conn->close();
+    header('Location:'.$redir);
+}
--- creer_compte.php
+++ creer_compte.php
@@ -1,31 +1,32 @@
-<?php include_once 'composant/header.php'; ?>
+<?php include_once 'composant/header.php'; 
+include_once 'composant/notification.php' ?>
 
 <form action='bdd/bddCreerCompte.php' method='POST'>
     <div class="form-group row mt-3">
         <label for="Nom" class="col-12 col-sm-2 col-form-label">Nom :</label>
         <div class="col-12 col-sm-10">
-            <input type="text" class="form-control" id="Nom" name="Nom" placeholder="Votre Nom">
+            <input type="text" class="form-control" id="Nom" name="Nom" placeholder="Votre Nom" required>
         </div>
     </div>
     
     <div class="form-group row mt-3">
         <label for="Prenom" class="col-12 col-sm-2 col-form-label">Prénom :</label>
         <div class="col-12 col-sm-10">
-            <input type="text" class="form-control" id="Prenom" name="Prenom" placeholder="Votre Prénom">
+            <input type="text" class="form-control" id="Prenom" name="Prenom" placeholder="Votre Prénom" required>
         </div>
     </div>
 
     <div class="form-group row mt-3">
         <label for="Email" class="col-12 col-sm-2 col-form-label">Email :</label>
         <div class="col-12 col-sm-10">
-            <input type="email" class="form-control" id="Email" name="Email" placeholder="email@groupe-esigelec.org">
+            <input type="email" class="form-control" id="Email" name="Email" placeholder="email@groupe-esigelec.org" required>
         </div>
     </div>
     
     <div class="form-group row mt-3">
         <label for="Password" class="col-12 col-sm-2 col-form-label">Mot de passe :</label>
         <div class="col-12 col-sm-10">
-            <input type="password" class="form-control" id="Password" name="Password" placeholder="Mot de passe">
+            <input type="password" class="form-control" id="Password" name="Password" placeholder="Mot de passe" required>
         </div>
     </div>
 
