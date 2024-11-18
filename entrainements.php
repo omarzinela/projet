94982826672d804a50da2f00f0c3e2f511a54a7d
omar <omar.zinelabidine@groupe-esigelec.org>
2024-11-18 14:05:05 +0100

    test

--- entrainements.php
+++ entrainements.php
@@ -4,51 +4,54 @@ include_once 'composant/notification.php';
 require_once 'bdd/bddEntrainements.php';
 
 if (!$res) :
-    $_SESSION["Err"] = '<p>Échec requête: ' . $conn->error . '</p>';
+  $_SESSION["Err"] = '<p>Échec requête: ' . $conn->error . '</p>';
 elseif ($res->num_rows == 0) :
-    $_SESSION["Info"] = '<p>Aucune course dans la bdd</p>';
+  $_SESSION["Info"] = '<p>Aucune course dans la bdd</p>';
 else :
-    while ($row = $res->fetch_assoc()) :
+  while ($row = $res->fetch_assoc()) :
 ?>
 
-<ul class="list-group list-group-flush flex-row border rounded mt-2">
-    <li class="list-group-item class_entrainement">
-        <div>
-        <?php echo $row['EntrainementNom']; ?> :
-        <ul class="mb-0">
-        <?php if (isset($_SESSION['UtilisateurId'])): ?>
-            <li> <?php echo date('Y/m/d',$row['EntrainementTimestamp']); ?> </li>
-            <li> <?php echo date('H:i',$row['EntrainementTimestamp']); ?> </li>
-            <li> <?php echo $row['Lieu']; ?> </li>
-        <?php endif;?>
-            <li> <?php echo $row['Categorie']; ?> </li>
-            <li> <?php echo $row['Description']; ?> </li>
-        </ul>
-        </div>
-    </li>
-    <?php if (isset($_SESSION['UtilisateurId'])): ?>
-    <div class="mt-auto">
-        <form action="bdd/inscriptionCourse.php" method = "POST">
-        <?php if (in_array($row['EntrainementId'],array_column($inscriptions,'EntrainementId'),true)): ?>
-            <input type="hidden" name="btnState" value="delete">
-            <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-color mb-2">Se désinscrire</button>
-        <?php elseif (@$nbParti[$row['EntrainementId']] < $row['MaxParticipants']): ?>
-            <input type="hidden" name="btnState" value="create">
-            <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-color mb-2">S'inscrire</button>
-        <?php endif;?>
-        <input type="hidden" name="source" value="../entrainements.php">
-        </form>
-    </div>
-    <?php endif;?>
+<div class="container mt-4">
+  <ul class="list-group list-group-flush border rounded">
 
-    <li> <div class="box mt-4 div_auto">
-        <div class="div div_auto">
-            <img src=" <?php echo $row['EntrainementThumbnail'] ?>" alt="Miniature de l'entraînement" class="img_course">
-        </div>
-    </div></li>
-</ul>
+  <li class="list-group-item">
+  <h5 class="mb-1"><?php echo $row['EntrainementNom']; ?></h5>
+  <ul class="list-unstyled">
+  <?php if (isset($_SESSION['UtilisateurId'])): ?>
+  <li><strong>Date:</strong> <?php echo date('Y/m/d', $row['EntrainementTimestamp']); ?></li>
+  <li><strong>Heure:</strong> <?php echo date('H:i', $row['EntrainementTimestamp']); ?></li>
+  <li><strong>Lieu:</strong> <?php echo $row['Lieu']; ?></li>
+  <?php endif; ?>
+  <li><strong>Catégorie:</strong> <?php echo $row['Categorie']; ?></li>
+  <li><strong>Description:</strong> <?php echo $row['Description']; ?></li>
+  </ul>
+  </li>
 
-<?php endwhile;
-$conn->close();
-endif;
+  <?php if (isset($_SESSION['UtilisateurId'])): ?>
+  <li class="list-group-item d-flex justify-content-between">
+  <form action="bdd/inscriptionCourse.php" method="POST">
+  <?php if (in_array($row['EntrainementId'], array_column($inscriptions, 'EntrainementId'), true)): ?>
+  <input type="hidden" name="btnState" value="delete">
+  <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-danger">Se désinscrire</button>
+  <?php elseif (@$nbParti[$row['EntrainementId']] < $row['MaxParticipants']): ?>
+  <input type="hidden" name="btnState" value="create">
+  <button type="submit" name="EntrainementId" value="<?php echo $row['EntrainementId'] ?>" class="btn btn-primary">S'inscrire</button>
+  <?php endif; ?>
+  <input type="hidden" name="source" value="../entrainements.php">
+  </form>
+  </li>
+  <?php endif; ?>
+
+  <li class="list-group-item">
+  <div class="text-center">
+  <img src="<?php echo $row['EntrainementThumbnail'] ?>" alt="Miniature de l'entraînement" class="img-fluid rounded img_train">
+  </div>
+  </li>
+  </ul>
+</div>
+
+<?php 
+  endwhile; 
+  $conn->close();
+endif; 
 include_once 'composant/footer.php';
